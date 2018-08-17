3c912631ba5915a6580fd0ee7cd74bdc07e2f3fb
jolivette <jolivette.jonathan@gmail.com>
2018-08-17 08:38:03 -0500

    Initial commit of js component of external js exercise

---
+++ js/external.js
@@ -0,0 +1,10 @@
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!")
+var mineToo = prompt("Favorite color?")
+alert(mineToo + ", that is my favorite color too!")
+
+var question = prompt("How much will you receive in payment for this week?");
+
+if(prompt !=null) {document.getElementById("question").innerHTML =
+    "So, you're making pretty decent money by taking home the following:" + question;
+}
